cd3901d6652b55a1d1149606496121f2029aed89
doc_bacardi <devnull@localhost>
2014-04-30 13:52:54 +0200

    Show test parameters.

--- src/main.c
+++ src/main.c
@@ -10,7 +10,6 @@
 #include "systime.h"
 #include "uprintf.h"
 
-#include "serial_vectors.h"
 
 /*-----------------------------------*/
 
@@ -20,8 +19,7 @@ TEST_RESULT_T test(TEST_PARAMETER_T *ptTestParam)
 	TEST_RESULT_T tTestResult;
 	SQITEST_PARAMETER_T *ptTestParams;
 	QSI_CFG_T tQsiCfg;
-	/* the load address of an SQI XIP image must be at the start of SQI ROM right after the boot block */
-	const unsigned char * const pucSqiXipAddress = (const unsigned char * const)(HOSTADDR(sqirom));
+	const unsigned char * const pucSqiRomAddress = (const unsigned char * const)(HOSTADDR(sqirom));
 
 
 	systime_init();
@@ -33,11 +31,14 @@ TEST_RESULT_T test(TEST_PARAMETER_T *ptTestParam)
 
 	/* Say "hi". */
 	uprintf("\f. *** SQI test start ***\n");
+	uprintf(". offset: 0x%08x\n", ptTestParams->ulOffset);
+	uprintf(". size:   0x%08x\n", ptTestParams->ulSize);
+	uprintf(". buffer: 0x%08x\n", ptTestParams->pucBuffer);
 
 	tTestResult = boot_sqi_xip(&tQsiCfg);
 	if( tTestResult==TEST_RESULT_OK )
 	{
-		memcpy(ptTestParams->pucBuffer, pucSqiXipAddress+ptTestParams->ulOffset, ptTestParams->ulSize);
+		memcpy(ptTestParams->pucBuffer, pucSqiRomAddress+ptTestParams->ulOffset, ptTestParams->ulSize);
 	}
 
 	return tTestResult;
